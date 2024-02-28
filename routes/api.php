64bfe5293e7b2690203af9584fc7cc524d132380
Achmad Baihaqi <haqiachd@gmail.com>
2024-02-28 21:58:15 +0700

    update api mobile

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Mobile\MobileAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group(['prefix'=>'/m'], function(){
+    Route::group(['prefix'=>'/test'], function(){
+        Route::get('/first', [MobileAuthController::class, 'first']);
+    });
+
+    Route::group(['prefix'=>'auth'], function(){
+        Route::get('/cekemail', [MobileAuthController::class, 'isExistEmail']);
+        Route::get('/cekphone', [MobileAuthController::class, 'isExistPhone']);
+    });
 });
