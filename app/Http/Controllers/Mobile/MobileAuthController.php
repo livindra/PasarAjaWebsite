e310629e8186d52cb606f2c88db0995bd7fb60aa
Achmad Baihaqi <haqiachd@gmail.com>
2024-03-16 15:30:29 +0700

    update mobile auth

--- app/Http/Controllers/Mobile/MobileAuthController.php
+++ app/Http/Controllers/Mobile/MobileAuthController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Mobile;
 
 use App\Http\Controllers\Controller;
+use App\Models\RefreshToken;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class MobileAuthController extends Controller
 {
@@ -40,16 +42,61 @@ class MobileAuthController extends Controller
         }
     }
 
+    public function isOnLogin(Request $request)
+    {
+        $email = $request->input('email');
+
+        $isExist = RefreshToken::select('email')->where('email', '=', $email)->limit(1)->exists();
+
+        if ($isExist) {
+            return response()->json(['status' => 'success', 'message' => 'Akun sedang login'], 200);
+        } else {
+            return response()->json(['status' => 'error', 'message' => 'Akun belum login'], 400);
+        }
+    }
+
     public function createUser(Request $request, User $user)
     {
+        
+        // validasi data
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => [
+                'required',
+                'string',
+                'min:8',
+                'max:30',
+                'regex:/[^\w\s]/',
+            ],
+            'full_name' => 'required|string|min:4|max:50',
+            'pin' => 'required|string|regex:/^\d{6}$/',
+            'phone_number' => 'nullable|string|regex:/^\d{9,15}$/',
+        ]);
+
+        // custom message
+        $customMsg = [
+            'email' => 'Email tidak valid',
+            'password' => 'Password tidak valid.',
+            'nama' => 'Nama tidak valid.',
+            'pin' => 'PIN tidak valid',
+            'phone_number' => 'Nomor HP tidak valid.',
+        ];
+        $validator->setCustomMessages($customMsg);
+
+        // cek validasi
+        if ($validator->fails()) {
+            return ['status'=>'error','message'=>$validator->errors()->first()];
+        }
+
+        // mendapatkan data
         $user->phone_number = $request->input('phone_number');
         $user->email = $request->input('email');
         $user->full_name = $request->input('full_name');
         $user->pin = Hash::make($request->input('pin'));
         $user->password = Hash::make($request->input('password'));
-        $user->level = "Pembeli";
         $user->is_verified = false;
 
+        // menyimpan data
         if ($user->save()) {
             return ['status' => 'success', 'message' => 'Akun berhasil dibuat'];
         } else {
@@ -65,11 +112,14 @@ class MobileAuthController extends Controller
         $request->input('password');
         $request->input('pin');
 
-        // cek phone registered
+        // cek email & no hp exist atau tidak
         $isExistEmail = json_decode($this->isExistEmail($request)->getContent(), true);
+        $isExistPhone = json_decode($this->isExistPhone($request)->getContent(), true);
 
         if ($isExistEmail['status'] === 'success') {
             return response()->json(['status' => 'error', 'message' => 'Email sudah terdaftar'], 400);
+        } else if ($isExistPhone['status'] === 'success') {
+            return response()->json(['status' => 'error', 'message' => 'Nomor HP sudah terdaftar'], 400);
         } else {
             $result = $this->createUser($request, $user);
             if ($request['status'] == 'error') {
@@ -82,17 +132,22 @@ class MobileAuthController extends Controller
 
     public function signinEmail(Request $request)
     {
+
         $email = $request->input('email');
         $password = $request->input('password');
 
-        // cek email 
+        // cek email exist atau tidak
         $isExist = json_decode($this->isExistEmail($request)->getContent(), true);
+        
+        // jika email tidak exist
         if ($isExist['status'] !== 'success') {
             return response()->json(['status' => 'error', 'message' => 'Email tidak terdaftar'], 400);
         }
 
-        // cek password
+        // get password
         $dbPass = User::select("password")->where('email', $email)->limit(1)->get();
+        
+        // jika password tidak cocok
         if (!password_verify($password, $dbPass[0]->password)) {
             return response()->json(['status' => 'error', 'message' => 'Password tidak cocok'], 400);
         }
@@ -107,18 +162,23 @@ class MobileAuthController extends Controller
         $phone = $request->input('phone_number');
         $pin = $request->input('pin');
 
-        // cek nomor hp
+        // cek nomor hp exist atau tidak
         $isExistPhone = json_decode($this->isExistPhone($request)->getContent(), true);
+        
+        // jika nomor hp tidak exist
         if ($isExistPhone['status'] !== 'success') {
             return response()->json(['status' => 'error', 'message' => 'Nomor Hp tidak terdaftar'], 400);
         }
 
-        // cek pin
+        // mendapatkan pin
         $dbPin = User::select("pin")->where('phone_number', $phone)->limit(1)->get();
+        
+        // jika pin tidak cocok
         if (!password_verify($pin, $dbPin[0]->pin)) {
             return response()->json(['status' => 'error', 'message' => 'PIN tidak cocok'], 400);
         }
 
+        // login & get user data
         $userData = User::select('*')->where('phone_number', $phone)->limit(1)->get();
         return response()->json(['status' => 'success', 'message' => 'Login Berhasil', 'data' => $userData[0]], 200);
     }
@@ -127,10 +187,14 @@ class MobileAuthController extends Controller
     {
         $email = $request->input('email');
 
+        // cek email exist atau tidak
         $isExistEmail = json_decode($this->isExistEmail($request)->getContent(), true);
+        
+        // jika email exist
         if ($isExistEmail['status'] !== 'success') {
             return response()->json(['status' => 'error', 'message' => 'Email tersebut belum terdaftar'], 400);
         } else {
+            // get user data
             $userData = User::select("*")->where('email', $email)->limit(1)->get();
             return response()->json(['status' => 'success', 'message' => 'Login Berhasil', 'data' => $userData[0]], 200);
         }
@@ -138,15 +202,43 @@ class MobileAuthController extends Controller
 
     public function changePassword(Request $request)
     {
+        // validasi data
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => [
+                'required',
+                'string',
+                'min:8',
+                'max:30',
+                'regex:/[^\w\s]/',
+            ],
+        ]);
+
+        // custom message
+        $customMsg = [
+            'email' => 'Email tidak valid',
+            'password' => 'Password tidak valid.',
+        ];
+        $validator->setCustomMessages($customMsg);
+
+        // cek validasi
+        if ($validator->fails()) {
+            return ['status'=>'error','message'=>$validator->errors()->first()];
+        }
+
+        // get data
         $email = $request->input('email');
         $newPass = $request->input('password');
         $passHash = Hash::make($newPass);
 
+        // cek nomor hp exist atau tidak
         $isExistEmail = json_decode($this->isExistEmail($request)->getContent(), true);
 
+        // jika email tidak exist
         if ($isExistEmail['status'] !== 'success') {
             return response()->json(['status' => 'success', 'message' => 'Email tidak terdaftar'], 400);
         } else {
+            // mengupdate password
             $update = User::select('email')->where('email', '=', $email)->update(['password' => $passHash]);
             if ($update) {
                 return response()->json(['status' => 'success', 'message' => 'Password berhasil diedit'], 200);
@@ -158,15 +250,37 @@ class MobileAuthController extends Controller
 
     public function changePin(Request $request)
     {
+        // validasi data
+        $validator = Validator::make($request->all(), [
+            'phone_number' => 'nullable|string|regex:/^\d{9,15}$/',
+            'pin' => 'required|string|regex:/^\d{6}$/',
+        ]);
+
+        // custom message
+        $customMsg = [
+            'phone_number' => 'Nomor HP tidak valid.',
+            'pin' => 'PIN tidak valid',
+        ];
+        $validator->setCustomMessages($customMsg);
+
+        // cek validasi
+        if ($validator->fails()) {
+            return ['status'=>'error','message'=>$validator->errors()->first()];
+        }
+
+        // get data
         $phone = $request->input('phone_number');
         $newPin = $request->input('pin');
         $pinHash = Hash::make($newPin);
 
+        // cek nomor hp exist atau tidak
         $isExistPhone = json_decode($this->isExistPhone($request)->getContent(), true);
 
+        // jika nomor hp tidak exist
         if ($isExistPhone['status'] !== 'success') {
             return response()->json(['status' => 'success', 'message' => 'Nomor HP tidak terdaftar'], 400);
         } else {
+            // menupdate pin
             $update = User::select('phone_number')->where('phone_number', '=', $phone)->update(['pin' => $pinHash]);
             if ($update) {
                 return response()->json(['status' => 'success', 'message' => 'PIN berhasil diedit'], 200);
