1c36601df2bd8cbdbc7ee740b2a6b1586fca1097
Achmad Baihaqi <haqiachd@gmail.com>
2024-03-18 15:04:46 +0700

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             ShopCategoriesSeeder::class,
             ProductCategoriesSeeder::class,
+            ShopSeeder::class,
         ]);
     }
 }
