2387e9ba5ceeb7ca4e4de23a7c98cb0ee2ee6484
Achmad Baihaqi <haqiachd@gmail.com>
2024-03-20 23:37:30 +0700

    update db

--- database/migrations/2024_02_25_170319_create_product_table.php
+++ database/migrations/2024_02_25_170319_create_product_table.php
@@ -17,15 +17,15 @@ return new class extends Migration
             $table->id('id_product');
             $table->unsignedBigInteger('id_shop');
             $table->unsignedBigInteger('id_cp_prod');
-            $table->string('product_name', 30);
+            $table->string('product_name', 50)->unique();
             $table->text('description')->nullable();
-            $table->smallInteger('total_sold');
-            $table->text('settings')->nullable()->default('{"is_recomended": false, "is_shown": false, "is_available": false}');
-            $table->enum('unit', ['gram', 'kilogram', 'liter']);
+            $table->smallInteger('total_sold')->default(0);
+            $table->text('settings')->nullable()->default('{"is_recommended": false, "is_shown": false, "is_available": false}');
+            $table->enum('unit', ['Gram', 'Kilogram', 'Ons', 'Kuintal', 'Ton', 'Liter', 'Milliliter', 'Sendok', 'Cangkir', 'Mangkok', 'Botol', 'Karton', 'Dus', 'Buah', 'Ekor']);
             $table->integer('selling_unit');
-            $table->integer('harga');
+            $table->integer('price');
             $table->text('promos')->nullable()->default('{"default_price": 80000, "promo_price": 70000, "promo_start": "2024-01-01", "promo_end": "2024-01-17"}');
-            $table->text('photo')->nullable()->default('product.png');
+            $table->text('photo');
             $table->timestamps();
             $table->foreign('id_shop')->references('id_shop')
                 ->on('0shops')->onDelete('cascade');
